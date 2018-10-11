b97a92ef0679b3973a80601b1fab88dfadc22624
nzaujk <joseph.nzau@andela.com>
2018-10-11 19:24:50 +0300

    add environment variables

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    COOKIE_KEY: process.env.SECRET,
+    facebookClientID: '1843874405733887',
+    facebookClientSecret: ''
+};
